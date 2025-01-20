cfebbc651a8950dc4e7f3d2525adc82c4a6a9352
kisanjena <kisanjena40@gmail.com>
2025-01-19 20:56:58 -0500

    add db schema

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin")
@@ -9,4 +11,11 @@ app.use("/user", userRouter);
 app.use("/course", courseRouter);
 app.use("/admin", adminRouter);
 
-app.listen(3000)
+
+async function main() {
+    mongoose.connect("mongodb+srv://kisanjena40:kisanjena123@cluster0.pe5nz.mongodb.net/Coursify")
+    app.listen(3000);
+    console.log("listening on port 3000")
+}
+
+main()
